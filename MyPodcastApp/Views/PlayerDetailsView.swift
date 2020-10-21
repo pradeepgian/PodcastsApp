e4e11f7c7240f0d8c8aabf452284e5cb7d1fa70e
Pradeep Gianchandani <pradeep.gian@gmail.com>
2020-10-21 17:28:09 +0530

    Implemented a functionality to support background playback. Also, added audio play/pause functionality in control center.

--- MyPodcastApp/Views/PlayerDetailsView.swift
+++ MyPodcastApp/Views/PlayerDetailsView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVKit
+import MediaPlayer
 
 class PlayerDetailsView: UIView {
     
@@ -16,12 +17,39 @@ class PlayerDetailsView: UIView {
             titleLabel.text = episode.title
             authorLabel.text = episode.author
             
+            //This method is used for displaying the playing track info on lock screen
+            setupNowPlayingInfo()
+            
+            //For supporting background audio mode, setupAudioSession() method has been implemented
+            //You also have to enable background audio mode in capabilities
+            //Enabling background audio in capabilities is not enough, you have to activate audio session
+            setupAudioSession()
+            
             playEpisode()
             
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             episodeImageView.sd_setImage(with: url)
             miniEpisodeImageView.sd_setImage(with: url)
+
+            miniEpisodeImageView.sd_setImage(with: url) { (image, _, _, _) in
+                let image = self.episodeImageView.image ?? UIImage()
+                let artworkItem = MPMediaItemArtwork(boundsSize: .zero, requestHandler: { (size) -> UIImage in
+                    return image
+                })
+                MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPMediaItemPropertyArtwork] = artworkItem
+            }
         }
+    }
+    
+    
+    //This method is used for displaying the playing track info on lock screen
+    fileprivate func setupNowPlayingInfo() {
+        var nowPlayingInfo = [String: Any]()
+        
+        nowPlayingInfo[MPMediaItemPropertyTitle] = episode.title
+        nowPlayingInfo[MPMediaItemPropertyArtist] = episode.author
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
     }
     
     fileprivate func playEpisode() {
@@ -88,31 +116,202 @@ class PlayerDetailsView: UIView {
         }
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        setupGestures()
+    //For supporting background audio mode, setupAudioSession() method has been implemented
+    //You also have to enable background audio mode in capabilities
+    //Enabling background audio in capabilities is not enough, you have to activate audio session
+    fileprivate func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch let sessionErr {
+            print("Failed to activate session:", sessionErr)
+        }
+    }
+    
+    
+    //This method helps in controlling audio playback through command center controls
+    //Calling togglePlayPauseCommand() method on command center object will help in controlling play pause via earphones
+    fileprivate func setupRemoteControl() {
+        UIApplication.shared.beginReceivingRemoteControlEvents()
         
-        observePlayerCurrentTime()
+        let commandCenter = MPRemoteCommandCenter.shared()
         
-        //CMTimeMake(1,10) actually means a value of 1 and a timescale of 10. They are a numerator and denominator, so it is 1/10 of a second
-        let time = CMTimeMake(value: 1, timescale: 3) //here the result is 1/3rd of a second
+        commandCenter.playCommand.isEnabled = true
+        commandCenter.playCommand.addTarget { (_) -> MPRemoteCommandHandlerStatus in
+            self.player.play()
+            self.playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            self.miniPlayPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            //If we dont call setupElapsedTime method, then player time keeps resetting to 0 during play/pause
+            self.setupElapsedTime(playbackRate: 1)
+            return .success
+        }
+        
+        commandCenter.pauseCommand.isEnabled = true
+        commandCenter.pauseCommand.addTarget { (_) -> MPRemoteCommandHandlerStatus in
+            self.player.pause()
+            self.playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            self.miniPlayPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            //If we dont call setupElapsedTime method, then player time keeps resetting to 0 during play/pause
+            self.setupElapsedTime(playbackRate: 0)
+            return .success
+        }
+        
+        //togglePlayPauseCommand() method on command center object will help in controlling play/pause via earphones
+        commandCenter.togglePlayPauseCommand.isEnabled = true
+        commandCenter.togglePlayPauseCommand.addTarget { (_) -> MPRemoteCommandHandlerStatus in
+            self.handlePlayPause()
+            return .success
+        }
+        
+        commandCenter.nextTrackCommand.addTarget {(_) -> MPRemoteCommandHandlerStatus in
+            self.handleNextTrack()
+            return .success
+        }
+        
+        commandCenter.previousTrackCommand.addTarget {(_) -> MPRemoteCommandHandlerStatus in
+            self.handlePrevTrack()
+            return .success
+        }
+        
+        
+//        commandCenter.nextTrackCommand.addTarget(self, action: #selector(handleNextTrack))
+//        commandCenter.previousTrackCommand.addTarget(self, action: #selector(handlePrevTrack))
+    }
+    
+    var playlistEpisodes = [Episode]()
+    
+    @objc fileprivate func handlePrevTrack() {
+        // 1. check if playlistEpisodes.count == 0 then return
+        // 2. find out current episode index
+        // 3. if episode index is 0, wrap to end of list somehow..
+              // otherwise play episode index - 1
+        if playlistEpisodes.isEmpty {
+            return
+        }
+        
+        let currentEpisodeIndex = playlistEpisodes.index { (ep) -> Bool in
+            return self.episode.title == ep.title && self.episode.author == ep.author
+        }
+        guard let index = currentEpisodeIndex else { return }
+        let prevEpisode: Episode
+        if index == 0 {
+            let count = playlistEpisodes.count
+            prevEpisode = playlistEpisodes[count - 1]
+        } else {
+            prevEpisode = playlistEpisodes[index - 1]
+        }
+        self.episode = prevEpisode
+    }
+    
+    @objc fileprivate func handleNextTrack() {
+        if playlistEpisodes.count == 0 {
+            return
+        }
+        
+        let currentEpisodeIndex = playlistEpisodes.index { (ep) -> Bool in
+            return self.episode.title == ep.title && self.episode.author == ep.author
+        }
+        
+        guard let index = currentEpisodeIndex else { return }
+        
+        let nextEpisode: Episode
+        if index == playlistEpisodes.count - 1 {
+            nextEpisode = playlistEpisodes[0]
+        } else {
+            nextEpisode = playlistEpisodes[index + 1]
+        }
+        
+        self.episode = nextEpisode
+    }
+    
+    fileprivate func setupElapsedTime(playbackRate: Float) {
+        let elapsedTime = CMTimeGetSeconds(player.currentTime())
+        //This property [MPNowPlayingInfoPropertyElapsedPlaybackTime] is used for displaying the elapsed time on left side of slider displayed on lock screen
+        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPNowPlayingInfoPropertyElapsedPlaybackTime] = elapsedTime
+        //Playback rate is speed of playback. If it is set to 2, speed of video will be 2x
+        //Here playback is being set to 0 when pause is tapped since we are facing the below issue
+        //Issue - incorrect elapsed time is displayed on lock screen, when player is paused, elapsed time changes to +5 seconds
+        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPNowPlayingInfoPropertyPlaybackRate] = playbackRate
+    }
+    
+    fileprivate func observeBoundaryTime() {
+        let time = CMTimeMake(value: 1, timescale: 3)
         let times = [NSValue(time: time)]
         
         // player has a reference to self
         // self has a reference to player
         player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
             print("Episode started playing")
-            //Requests invocation of this block when playback traverses 1/3 of a second during normal playback.
             self?.enlargeEpisodeImageView()
+            self?.setupLockscreenDuration()
         }
+    }
+    
+    //
+    fileprivate func setupLockscreenDuration() {
+        guard let duration = player.currentItem?.duration else { return }
+        let durationSeconds = CMTimeGetSeconds(duration)
+        //This property [MPMediaItemPropertyPlaybackDuration] is used for displaying the total duration of track on right side of slider displayed on lock screen
+        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPMediaItemPropertyPlaybackDuration] = durationSeconds
+    }
+    
+    
+    //This method has been implemented to handle phone call while playing audio
+    fileprivate func setupInterruptionObserver() {
+        // don't forget to remove self on deinit
+        NotificationCenter.default.addObserver(self, selector: #selector(handleInterruption), name: AVAudioSession.interruptionNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: AVAudioSession.interruptionNotification, object: nil)
+    }
+    
+    //This method helps in handling phone call while playing audio
+    @objc fileprivate func handleInterruption(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        
+        guard let type = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt else { return }
+        
+        if type == AVAudioSession.InterruptionType.began.rawValue {
+            print("Interruption began")
+            
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            miniPlayPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            
+        } else {
+            print("Interruption ended...")
+            //Issue - when we play the audio and pause it, go to background and come back to foreground, audio resumes automatically
+            //Too avoid above issue, we check shouldResume flag if interruption has ended and if app can resume audio session
+            guard let options = userInfo[AVAudioSessionInterruptionOptionKey] as? UInt else { return }
+            
+            if options == AVAudioSession.InterruptionOptions.shouldResume.rawValue {
+                player.play()
+                playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+                miniPlayPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            }
+         
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //This method helps in controlling audio playback through command center controls
+        //Calling togglePlayPauseCommand() method on command center object will help in controlling play pause via earphones
+        setupRemoteControl()
+        
+        setupGestures()
+        
+        setupInterruptionObserver()
+        
+        observePlayerCurrentTime()
+        
+        observeBoundaryTime()
     }
     
     static func initFromNib() -> PlayerDetailsView {
         return Bundle.main.loadNibNamed("PlayerDetailsView", owner: self, options: nil)?.first as! PlayerDetailsView
-    }
-    
-    deinit {
-        print("PlayerDetailsView memory being reclaimed...")
     }
     
     //MARK:- IB Actions and Outlets
@@ -144,6 +343,9 @@ class PlayerDetailsView: UIView {
         let durationInSeconds = CMTimeGetSeconds(duration)
         let seekTimeInSeconds = Float64(percentage) * durationInSeconds
         let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
+        
+        //This property [MPNowPlayingInfoPropertyElapsedPlaybackTime] is used for displaying the elapsed time on left side of slider displayed on lock screen
+        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPNowPlayingInfoPropertyElapsedPlaybackTime] = seekTimeInSeconds
         
         player.seek(to: seekTime)
     }
@@ -211,11 +413,13 @@ class PlayerDetailsView: UIView {
             playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
             miniPlayPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
             enlargeEpisodeImageView()
+            self.setupElapsedTime(playbackRate: 1)
         } else {
             player.pause()
             playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
             miniPlayPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
             shrinkEpisodeImageView()
+            self.setupElapsedTime(playbackRate: 0)
         }
     }
     
